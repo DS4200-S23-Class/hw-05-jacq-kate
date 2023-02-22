89a511fae162840282e621e1487687b11ca2cdc6
Kate Lanman <katelanman@Kates-MacBook-Pro.local>
2023-02-21 19:22:07 -0500

    bar chart done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // declare constants
-const FRAME_HEIGHT = 300;
-const FRAME_WIDTH = 300;
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 400;
 const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
 
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom
@@ -55,7 +55,7 @@ d3.csv("data/scatter-data.csv").then((data) => {
 
 	// mouseover
 	function handleMouseover(event, d) {
-		d3.select(this).style("fill", "blue");
+		d3.select(this).style("fill", "slateblue");
 	}
 
 	// mouseleave
@@ -131,7 +131,6 @@ d3.select("#vis2")
 
 // bar plot from bar data
 d3.csv("data/bar-data.csv").then((data) => {
-	console.log(data)
 	const MAX_Y = d3.max(data, (d) => {return parseInt(d.amount)});
 	const Y_SCALE = d3.scaleLinear()
 						.domain([0, MAX_Y + 1])
@@ -142,23 +141,26 @@ d3.csv("data/bar-data.csv").then((data) => {
 						.domain([0, MAX_X + 1])
 						.range([0, VIS_WIDTH]);
 
+	const TICK_SCALE = d3.scaleBand(data.map((d) => d.category), 
+									[0, FRAME_WIDTH - MARGINS.right])
+							.paddingInner(0.01);
+
 	// plot
 	FRAME2.selectAll("bars")
 			.data(data)
 			.enter()
 			.append("rect")
 				.attr("class", "bar")
-				.attr("x", (d, i) => {return X_SCALE(i) + MARGINS.left})
-				.attr("y", (d) => {return FRAME_HEIGHT - Y_SCALE(d.amount) - MARGINS.top})
-				.attr("width", 30)
-				.attr("height", (d) => {return Y_SCALE(d.amount) + MARGINS.top})
-				.style("fill", "blue");
+				.attr("x", (d, i) => {return X_SCALE(i + (i/6) + 0.15) + MARGINS.left})
+				.attr("y", (d) => {return Y_SCALE(d.amount) + MARGINS.top})
+				.attr("width", 40)
+				.attr("height", (d) => {return VIS_HEIGHT - Y_SCALE(d.amount)})
+				.style("fill", "lightblue");
 
 	// x axis
 	FRAME2.append("g")
-			.attr("transform", 
-					"translate(" + MARGINS.left + "," + (FRAME_HEIGHT - MARGINS.top) + ")")
-				.call(d3.axisBottom(X_SCALE).ticks(6))
+			.attr("transform", "translate(" + MARGINS.left + "," + (FRAME_HEIGHT - MARGINS.top) + ")")
+				.call(d3.axisBottom(TICK_SCALE))
 					.attr("font-size", "15px"); 
 
 	// y axis
@@ -167,6 +169,37 @@ d3.csv("data/bar-data.csv").then((data) => {
 					"translate(" + MARGINS.left + "," + MARGINS.top + ")")
 				.call(d3.axisLeft(Y_SCALE).ticks(10))
 					.attr("font-size", "15px");
+
+	// tooltip
+	const TOOLTIP = d3.select("#vis2")
+						.append("div")
+							.attr("class", "tooltip")
+							.style("opacity", 0);
+
+	// mouseover
+	function handleMouseover(event, d) {
+		d3.select(this).style("fill", "slateblue");
+		TOOLTIP.style("opacity", 1);
+	}
+
+	// mousemove
+	function handleMousemove(event, d) {
+		TOOLTIP.html("Category: " + d.category + "<br>Amount: " + d.amount)
+				.style("left", (event.pageX + 10) + "px")
+				.style("top", (event.pageY - 50) + "px");
+	}
+
+	// mouseleave
+	function handleMouseleave(event, d) {
+		d3.select(this).style("fill", "lightblue");
+		TOOLTIP.style("opacity", 0);
+	}
+
+	// add event listeners for bars
+	FRAME2.selectAll(".bar")
+			.on("mouseover", handleMouseover)
+			.on("mousemove", handleMousemove)
+			.on("mouseleave", handleMouseleave);
 });
 
 
